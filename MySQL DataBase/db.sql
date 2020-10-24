09050e348de737f9b76cc86937fce951e5eb5838
Krystian Śliwiński <72706877+Infor-Tech@users.noreply.github.com>
2020-10-24 12:36:57 +0200

    Update db.sql

--- MySQL DataBase/db.sql
+++ MySQL DataBase/db.sql
@@ -6,24 +6,24 @@ USE arduino;
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs(
     logdate TIMESTAMP NOT NULL,
-    room char(4) NOT NULL,
-    pinused char(4) NOT NULL,
+    room CHAR(4) NOT NULL,
+    pinused CHAR(4) NOT NULL,
     success BIT NOT NULL,
     PRIMARY KEY(logdate, room) 
 );
 
 DROP TABLE IF EXISTS room_1001;
 CREATE TABLE room_1001(
-    date_from datetime NOT NULL,
-    date_to datetime NOT NULL,
+    date_from DATETIME NOT NULL,
+    date_to DATETIME NOT NULL,
     pin char(4),
     PRIMARY KEY(date_from, date_to)
 );
 
 DROP TABLE IF EXISTS room_2001;
 CREATE TABLE room_2001(
-    date_from datetime NOT NULL,
-    date_to datetime NOT NULL,
+    date_from DATETIME NOT NULL,
+    date_to DATETIME NOT NULL,
     pin char(4) NOT NULL,
     PRIMARY KEY(date_from, date_to)
 );
